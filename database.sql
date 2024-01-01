9c1db0a711b9b1256e08a6391d38599eb0d566cd
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-01 20:57:14 +0700

    *Spring Data JPA - Repository

--- database.sql
+++ database.sql
@@ -1,1 +1,9 @@
-SELECT * FROM test;
+--SELECT * FROM test;
+
+--create TABLE categories(
+--id BIGINT NOT NULL AUTO_INCREMENT,
+--name VARCHAR(100) NOT NULL,
+--PRIMARY KEY (id)
+--)ENGINE=InnoDb;
+--COMMIT;
+SELECT * FROM categories;

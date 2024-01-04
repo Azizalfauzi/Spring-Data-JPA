9f05e76c77e7a8c47db1d6d18920271eca58a642
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-04 20:27:54 +0700

    *Spring Data JPA - JPA Query Method

--- src/test/java/zuhaproject/spring/jpa/CategoryServiceTest.java
+++ src/test/java/zuhaproject/spring/jpa/CategoryServiceTest.java
@@ -1,15 +1,23 @@
 package zuhaproject.spring.jpa;
 
+import lombok.val;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import zuhaproject.spring.jpa.entity.Category;
+import zuhaproject.spring.jpa.repository.CategoryRepository;
 import zuhaproject.spring.jpa.service.CategoryService;
+
+import java.util.List;
 
 @SpringBootTest
 public class CategoryServiceTest {
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Test
     void succes() {
@@ -30,5 +38,17 @@ public class CategoryServiceTest {
         Assertions.assertThrows(RuntimeException.class, () -> {
             categoryService.createCategories();
         });
+    }
+
+    @Test
+    void queryMethod() {
+        Category category = categoryRepository.findFirstByNameEquals("GADGET Murah").orElse(null);
+        Assertions.assertNotNull(category);
+        Assertions.assertEquals("GADGET Murah", category.getName());
+
+        List<Category> categories = categoryRepository.findAllByNameLike("%GADGET%");
+        Assertions.assertEquals(2, categories.size());
+        Assertions.assertEquals("GADGET Murah", categories.get(0).getName());
+
     }
 }

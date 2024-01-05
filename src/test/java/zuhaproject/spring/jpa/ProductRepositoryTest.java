bec9dd875bd5101f23efbf447c0a641b5f2238df
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-05 13:43:53 +0700

    *Spring Data JPA - JPA Paging Result

--- src/test/java/zuhaproject/spring/jpa/ProductRepositoryTest.java
+++ src/test/java/zuhaproject/spring/jpa/ProductRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -66,13 +67,19 @@ public class ProductRepositoryTest {
     void pageable() {
 //        Page 0
         Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Order.desc("id")));
-        List<Product> products = productRepository.findAllByCategory_Name("Gadget Murah", pageable);
-        Assertions.assertEquals(1, products.size());
-        Assertions.assertEquals("APPLE IPHONE 13 Pro-Max", products.get(0).getName());
+        Page<Product> products = productRepository.findAllByCategory_Name("Gadget Murah", pageable);
+        Assertions.assertEquals(1, products.getContent().size());
+        Assertions.assertEquals(0, products.getNumber());
+        Assertions.assertEquals(2, products.getTotalElements());
+        Assertions.assertEquals(2, products.getTotalPages());
+        Assertions.assertEquals("APPLE IPHONE 13 Pro-Max", products.getContent().get(0).getName());
 //      Page 1
         pageable = PageRequest.of(1, 1, Sort.by(Sort.Order.desc("id")));
         products = productRepository.findAllByCategory_Name("Gadget Murah", pageable);
-        Assertions.assertEquals(1, products.size());
-        Assertions.assertEquals("APPLE IPHONE 14 Pro-Max", products.get(0).getName());
+        Assertions.assertEquals(1, products.getContent().size());
+        Assertions.assertEquals(1, products.getNumber());
+        Assertions.assertEquals(2, products.getTotalElements());
+        Assertions.assertEquals(2, products.getTotalPages());
+        Assertions.assertEquals("APPLE IPHONE 14 Pro-Max", products.getContent().get(0).getName());
     }
 }
